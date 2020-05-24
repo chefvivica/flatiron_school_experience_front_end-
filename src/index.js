b6de2d66a965127c1925b9d685965dcd3f1cd2b0
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-05-24 09:57:30 -0400

    leaderboardandsignin

--- src/index.js
+++ src/index.js
@@ -2,15 +2,40 @@ document.addEventListener('DOMContentLoaded', function(e){
 
 const ursersUrl = 'http://localhost:3000/users'
 const avatarsUrl = 'http://localhost:3000/avatars'
+const board = document.querySelector('#board')
 
-
-function leaderBoard(array){
-   
-
+// clear leaderboard screen goes to sign in prompt?
+function clearLeaderBoard(){
+    const header = document.querySelector('h1').innerHTML = 'Welcome to flatiron-school-experience-game'
+    board.innerHTML = '<h3> Please Create New Username or Sign In </h3>' 
+    const signIn = document.createElement('input')
+    signIn.setAttribute('placeholder', 'your name')
+    const button = document.createElement('button', 'submit')
+    button.innerHTML = 'Submit'
+    board.appendChild(signIn)
+    board.appendChild(button)
+   //// fetch request for the avatars matching this persons id
+   //// or post reqest creating user 
 }
 
+/// our leader board can be up for like 5-7 seconds before user is prompted to sign in
+function leaderBoard(array){
+   document.querySelector('h1').innerHTML = 'Alumni Board'
+   array.forEach(person => {
+      const whoPlayed = person.username
+      person.avatars.forEach(avatar => {
+          line = document.createElement('h3')
+          line.setAttribute('dataset', `${avatar.skills}`)
+          line.innerText = `${whoPlayed} as ${avatar.name} scored ${avatar.points} points gained ${avatar.skills} skills in ${avatar.turns} turns`
+          board.append(line)
+      })
+  })
+}
 
-fetch(ursersUrl).then(res => res.json()).then(users => console.log(users))
+fetch(ursersUrl).then(res => res.json()).then(users => leaderBoard(users))
+
+//leaderboard only lasts 6 secons on the page 
+window.setTimeout(clearLeaderBoard, 6000)
 
 
 
