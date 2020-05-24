5c767df89b17840d7dacdcd1b432de2c95e57fe5
Vivica <Ma>
2020-05-24 16:58:18 -0400

    before the game starrt

--- src/user.js
+++ src/user.js
@@ -1,3 +1,10 @@
+//use this funcation to change background
+const background = (imgUrl) =>{
+  document.body.style.backgroundImage = `url(${imgUrl})`;
+}
+
+document.body.style.backgroundImage =  "url('https://secure.meetupstatic.com/photos/event/8/c/4/f/600_467315919.jpeg')";
+
 const logInForm = document.querySelector('#logInForm')
 document.addEventListener('click',e=>{
   e.preventDefault()
@@ -13,7 +20,37 @@ document.addEventListener('click',e=>{
     break;
 
     case("log_in_btn"):
-    
+    background("https://media3.giphy.com/media/yoJC2xC7FRU3D7yguY/giphy.gif?cid=ecf05e47ffbf69ba614324c6a378bfc815e1f8715f18e5c1&rid=giphy.gif")
+    const btn = document.querySelector('#log_in').style.display = "none"
+    logInForm.style.display ="none"
+    const pickAvatarBtn = document.createElement('button')
+    pickAvatarBtn.className = 'pickAvatarBtn'
+    pickAvatarBtn.textContent = 'Pick Your Avatar'
+    document.body.append(pickAvatarBtn)
+    break;
+
+    case("pickAvatarBtn"):
+    document.body.style.backgroundImage ='none'
+    document.body.style.backgroundColor = "#9edee6"
+    const avatarBtn = document.querySelector(".pickAvatarBtn").style.display = "none"
+    const avatarDiv = document.createElement('div')
+    avatarDiv.className = 'avatarSelectDiv'
+    avatarDiv.innerHTML =`
+    <span class="avatar_pic"><img src = './img/avt1.png'/></span>
+    <span class="avatar_pic"><img src = './img/avt2.png'/></span>
+    <span class="avatar_pic"><img src = './img/avt3.png'/></span>
+    <span class="avatar_pic"><img src = './img/avt4.png'/></span>
+    <span class="avatar_pic"><img src = './img/avt5.png'/></span>
+    <span class="avatar_pic"><img src = './img/avt6.png'/></span>
+    <span class="avatar_pic"><img src = './img/avt7.png'/></span>
+   
+
+    `
+    document.body.append(avatarDiv)
   }
+
+  // const getAvatar = () =>{
+  //   fetch()
+  // }
 
 })
